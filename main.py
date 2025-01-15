6735a12c7640b6b409c6a7ea55d71334d112b447
arnavdeepaware <arnav.deepaware@gmail.com>
2025-01-15 01:17:18 -0500

    Added CORS and Front End

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import datetime
 import json
 from flask import Flask, jsonify, request
+from flask_cors import CORS 
 import sqlite3
 
 #Flask
 app = Flask(__name__)
+CORS(app)
 
 #sqlite 
 def get_db():
